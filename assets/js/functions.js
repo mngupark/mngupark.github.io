1e38e9423184d0b0195066598608b422f9d62cb5
Gyuhub <alsrb0820@gmail.com>
2023-01-04 22:12:17 +0900

    add a dark mode toggle button

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,3 +10,26 @@ function toggle_category(id) {
           e_btn.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--button-bg-color');
        }
 }
+
+let cur_degree = 0;
+
+function toggle_theme() {
+   let currentMode = document.documentElement.getAttribute('data-theme');
+   var icon = document.getElementById('theme-icon');
+   if (currentMode == 'dark-poole') {
+      document.documentElement.setAttribute('data-theme', 'light-poole');
+		window.localStorage.setItem('theme', 'light');
+      icon.src = '/assets/sun.ico';
+		// document.getElementById("theme_btn").innerHTML = "Dark Mode";
+   }
+   else {
+      document.documentElement.setAttribute('data-theme', 'dark-poole');
+		window.localStorage.setItem('theme', 'dark');
+      icon.src = '/assets/moon.ico';
+		// document.getElementById("theme_btn").innerHTML = "Light Mode";
+   }
+   if (cur_degree == 360) cur_degree = 0;
+   else cur_degree += 360;
+   icon.style.transform = 'rotate(' + cur_degree + 'deg)';
+   icon.style.transition = 'all 1s';
+}
