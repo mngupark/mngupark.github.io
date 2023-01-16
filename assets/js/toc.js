d40db970fde28cceccb5d36f565d8eac90e93cad
Gyuhub <alsrb0820@gmail.com>
2023-01-16 23:44:19 +0900

    encode url of heading ids in JS

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const observer = new IntersectionObserver(entries => {
 		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
+			const id = encodeURI(entry.target.getAttribute('id'));
             if (entry.intersectionRatio > 0) {
                 document.querySelector(`.sidebar li a[href="#${id}"]`).parentElement.classList.add('active');
 			} else {
@@ -11,9 +11,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
-	// Track only h1 level headers that have an `id` applied
-	document.querySelectorAll('h1[id]').forEach((header) => {
-		observer.observe(header);
+	// Track only headings that have an `id` applied
+	document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').forEach((heading) => {
+		observer.observe(heading);
 	});
 	
 });
