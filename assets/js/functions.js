3874ecb20d358b53a8b51f5a298b29d7bb1b3735
Gyuhub <alsrb0820@gmail.com>
2023-01-18 22:50:28 +0900

    add fixed navgation bar

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,18 +1,18 @@
 function toggleCategory(id) {
-    var e_ul = document.getElementById(id);
-    var e_btn = document.getElementById(id + '_btn');
-       if(e_ul.style.visibility == 'hidden') {
-          e_ul.style.visibility = 'visible';
-          e_ul.style.opacity = 1;
-          e_ul.style.height = 'auto';
-          e_btn.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--button-hover-bg-color');
-       }
-       else {
-          e_ul.style.visibility = 'hidden';
-          e_ul.style.opacity = 0;
-          e_ul.style.height = '0';
-          e_btn.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--button-bg-color');
-       }
+   var e_ul = document.getElementById(id);
+   var e_btn = document.getElementById(id + '_btn');
+   if(e_ul.style.visibility == 'hidden') {
+      e_ul.style.visibility = 'visible';
+      e_ul.style.opacity = 1;
+      e_ul.style.height = 'auto';
+      e_btn.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--button-hover-bg-color');
+   }
+   else {
+      e_ul.style.visibility = 'hidden';
+      e_ul.style.opacity = 0;
+      e_ul.style.height = '0';
+      e_btn.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--button-bg-color');
+   }
 }
 
 let cur_degree = 0;
@@ -21,7 +21,6 @@ function toggleTheme() {
    let currentMode = localStorage.getItem('data-theme');
    let iframe = document.querySelector("iframe");
    let msg = JSON.parse(localStorage.msg);
-   console.log(msg);
    var icon = document.getElementById('theme-icon');
    if (currentMode == 'dark-poole') {
       document.documentElement.setAttribute('data-theme', 'light-poole');
@@ -43,4 +42,43 @@ function toggleTheme() {
    icon.style.transition = 'all 1s';
    localStorage.setItem('msg', JSON.stringify(msg));
    iframe.contentWindow.postMessage(msg, "https://utteranc.es");
+}
+
+// Hide header on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = document.getElementsByClassName('header')[0].clientHeight;
+
+addEventListener('scroll', (event) => {
+  didScroll = true;
+});
+
+setInterval(function() {
+   if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+   }
+}, 250);
+
+function hasScrolled() {
+   var st = window.scrollY;
+    
+   // Make scroll more than delta
+   if(Math.abs(lastScrollTop - st) <= delta)
+       return;
+    
+   // If scrolled down and past the navbar, add class .nav-up.
+   if (st > lastScrollTop && st > navbarHeight){
+      // Scroll Down
+      document.getElementsByClassName('header')[0].classList.add('nav-up');
+      document.getElementsByClassName('sidebar')[0].style.top = '2.5rem';
+   } else {
+      // Scroll Up
+      if(st + window.innerHeight < document.documentElement.scrollHeight) {
+         document.getElementsByClassName('header')[0].classList.remove('nav-up');
+         document.getElementsByClassName('sidebar')[0].style.top = '5rem';
+      }
+   }
+   lastScrollTop = st;
 }
--- assets/js/toc.js
+++ assets/js/toc.js
@@ -6,7 +6,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			if (document.querySelector(`.sidebar li a[href="#${id}"]`) == null)
 				id = decodeURI(id);
             if (entry.intersectionRatio > 0) {
-				console.log(document.querySelector(`.sidebar li a[href="#${id}"]`));
                 document.querySelector(`.sidebar li a[href="#${id}"]`).parentElement.classList.add('active');
 			} else {
 				document.querySelector(`.sidebar li a[href="#${id}"]`).parentElement.classList.remove('active');
