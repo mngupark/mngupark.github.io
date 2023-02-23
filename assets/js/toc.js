b35f48611b1a07ce311702dc80cccb3d9efe6a78
Mingyu Park <m1n9yu@kaist.ac.kr>
2023-02-24 00:29:58 +0900

    add remaining headers on toc

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,21 +1,44 @@
-window.addEventListener('DOMContentLoaded', () => {
+class Queue {
+	constructor() {
+	  	this._arr = [];
+	  	this._lastToc = new String();
+	}
+	enqueue(item) {
+	  	this._arr.push(item);
+	}
+	dequeue() {
+	  	return this._arr.shift();
+	}
+}
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			var id = encodeURI(entry.target.getAttribute('id'));
-			if (document.querySelector(`.sidebar li a[href="#${id}"]`) == null)
-				id = decodeURI(id);
-            if (entry.intersectionRatio > 0) {
-                document.querySelector(`.sidebar li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`.sidebar li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
+const activatedToc = new Queue();
+const headings = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
+const sidebarAnchors = document.querySelectorAll('.sidebar li a');
+const headingIds = [...headings].map((heading) => heading.id);
+
+const observerCallback = (entries) => {
+	entries.forEach(entry => {
+		var id = entry.target.id;
+        if (entry.isIntersecting == true && entry.intersectionRatio > 0) {
+			if (activatedToc._arr.length == 0 && activatedToc._lastToc.length != 0)
+				sidebarAnchors[headingIds.indexOf(activatedToc._lastToc)].parentElement.classList.remove('active');
+			activatedToc.enqueue(decodeURI(id));
+			sidebarAnchors[headingIds.indexOf(id)].parentElement.classList.add('active');
+		} else {
+			if (activatedToc._arr.find((element) => element == id) != undefined)
+				activatedToc._lastToc = activatedToc.dequeue();
+			if (activatedToc._arr.length != 0)
+				sidebarAnchors[headingIds.indexOf(id)].parentElement.classList.remove('active');
+		}
 	});
+};
 
+const observer = new IntersectionObserver(observerCallback);
+
+window.addEventListener('load', () => {
 	// Track only headings that have an `id` applied
-	document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').forEach((heading) => {
+	headings.forEach((heading) => {
 		observer.observe(heading);
 	});
-	
+
 });
